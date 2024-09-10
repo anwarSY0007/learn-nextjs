e3b5ece1ac22b75fdc209b11aa3294a33099f5ed
anwarSY0007 <anwar.sy0007@gmail.com>
2024-09-10 15:25:37 +0700

    feat: initialize app

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const CustomersPage = () => {
+  return <div className="">CustomersPage</div>;
+};
+
+export default CustomersPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const InvoicesPage = () => {
+  return <div className="">InvoicesPage</div>;
+};
+
+export default InvoicesPage;
